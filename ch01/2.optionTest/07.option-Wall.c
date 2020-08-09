640fdcf4ca9f7f218ba66c27b96c20acbec02ebd
shkim <shkim.hi@gmail.com>
2020-08-09 22:29:59 +0900

    lou day1

---
+++ ch01/2.optionTest/07.option-Wall.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int gv1 = 10, gv2;
+
+int main(void)  
+{
+    int lv1, lv2, lv3=30;
+    const int lv4=10;
+    short *p;
+
+    printf("Main Start..\n");
+    lv1 = 10;
+    printf("Main End...\n");
+
+    return 0;
+}
